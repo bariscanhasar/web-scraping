cac3003e86f80169c792914b242e318d400f7e66
kajmergroup <bariscanhasar@icloud.com>
2023-06-15 14:27:47 +0300

    2

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg');
 
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'arabam_db',
-    password: '123',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database:  process.env.DB_NAME,
+    password:  process.env.DB_PASSWORD,
+    port:  process.env.DB_PORT,
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0,
 });
